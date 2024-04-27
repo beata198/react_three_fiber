83dec8fdcd34151a9f1251d923f413ba99a64dea
beata198 <b.lublewska@gmail.com>
2024-04-27 14:11:50 +0200

    create and group cubes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
+import { Canvas } from "@react-three/fiber";
 import "./App.css";
 
 function App() {
+  const Cube = ({ position, size, color }) => {
+    return (
+      <mesh position={position}>
+        <boxGeometry args={size} />
+        <meshStandardMaterial color={color} />
+      </mesh>
+    );
+  };
   return (
-    <>
-      <p>react fiber</p>
-    </>
+    <Canvas>
+      <directionalLight position={[0, 0, 2]} intensity={0.9} />
+      <ambientLight intensity={0.7} />
+      <group position={[0, -1, 0]}>
+        <Cube position={[1, 0, 0]} color={"green"} size={[1, 1, 1]} />
+        <Cube position={[-1, 0, 0]} color={"red"} size={[1, 1, 1]} />
+        <Cube position={[-1, 2, 0]} color={"yellow"} size={[1, 1, 1]} />
+        <Cube position={[1, 2, 0]} color={"pink"} size={[1, 1, 1]} />
+      </group>
+    </Canvas>
   );
 }
 
