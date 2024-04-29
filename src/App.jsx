3705a51596284acf0767e99868e6c9d41599b01e
beata198 <b.lublewska@gmail.com>
2024-04-29 17:27:40 +0200

    animate cube

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useFrame } from "@react-three/fiber";
 import "./App.css";
+import { useRef } from "react";
 
 function App() {
   const Cube = ({ position, size, color }) => {
+    const ref = useRef();
+    useFrame((state, delta) => {
+      ref.current.rotation.y += delta;
+      ref.current.rotation.x += delta;
+      ref.current.position.z += Math.sin(state.clock.elapsedTime) * 0.03;
+    });
     return (
-      <mesh position={position}>
+      <mesh position={position} ref={ref}>
         <boxGeometry args={size} />
         <meshStandardMaterial color={color} />
       </mesh>
@@ -14,12 +21,14 @@ function App() {
     <Canvas>
       <directionalLight position={[0, 0, 2]} intensity={0.9} />
       <ambientLight intensity={0.7} />
-      <group position={[0, -1, 0]}>
+
+      {/* <group position={[0, -1, 0]}>
         <Cube position={[1, 0, 0]} color={"green"} size={[1, 1, 1]} />
         <Cube position={[-1, 0, 0]} color={"red"} size={[1, 1, 1]} />
         <Cube position={[-1, 2, 0]} color={"yellow"} size={[1, 1, 1]} />
         <Cube position={[1, 2, 0]} color={"pink"} size={[1, 1, 1]} />
-      </group>
+      </group> */}
+      <Cube position={[0, 0, 0]} color={"green"} size={[1, 1, 1]} />
     </Canvas>
   );
 }
