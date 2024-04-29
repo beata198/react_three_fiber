a713363af971b4aa19098d519e733cff1e311b97
beata198 <b.lublewska@gmail.com>
2024-04-29 18:36:17 +0200

    interactions with mouse

--- src/assets/components/TorusKnot.jsx
+++ src/assets/components/TorusKnot.jsx
@@ -1,18 +1,25 @@
 import { useFrame } from "@react-three/fiber";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 const TorusKnot = ({ position, size, color }) => {
   const ref = useRef();
+  const [isHovered, setIsHovered] = useState(false);
+
   useFrame((state, delta) => {
-    ref.current.rotation.y += delta;
-    ref.current.rotation.x += delta;
-    ref.current.position.z += Math.sin(state.clock.elapsedTime) * 0.01;
+    const speed = isHovered ? 1 : 0.1;
+    ref.current.rotation.y += delta * speed;
   });
 
   return (
-    <mesh position={position} ref={ref}>
+    <mesh
+      position={position}
+      ref={ref}
+      onPointerEnter={(e) => (e.stopPropagation(), setIsHovered(true))}
+      onPointerLeave={(e) => setIsHovered(false)}
+      scale={isHovered ? 1.2 : 1}
+    >
       <torusKnotGeometry args={size} />
-      <meshStandardMaterial color={color} />
+      <meshStandardMaterial color={isHovered ? color : "lightblue"} />
     </mesh>
   );
 };
